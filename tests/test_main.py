8c7747204deee22ac2e14ccc66aecbc03c53b6e6
ankushkgupta2 <ankushkgupta@deloitte.com>
2023-06-20 14:18:22 -0400

    test

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,14 +4,14 @@ import glob
 import shutil
 import pandas as pd
 
-# import the module for splitting fasta
+# import the module for general utility functions for annotation
 import sys
 sys.path.append(".")
 sys.path.append("..")
 from bin.annotation_utility import MainUtility as main_util
 
-# just initialize the util function globally 
-util = UtilityFunctions()
+# import the module for utility functions for testin
+from utility import UtilityFunctions as util
 
 
 @pytest.mark.parametrize("run_method", ["conda", "docker"])
@@ -333,45 +333,6 @@ class Submission(OutputChecks):
         if initial_or_update == 'update' or initial_or_update == 'both':
             assert os.path.exists(f"{directory}/update_submit_info/{sample_name}_update_submit_info.txt")
             assert os.path.exists(f"{directory}/update_submit_info/{sample_name}_update_terminal_output.txt")
-
-
-class UtilityFunctions():
-    def __init__(self):
-        self.root_dir = '/'.join(__file__.split('/')[:-2])
-        self.ext_util = main_util()
-
-    def call_submission(self, output_dir, meta_dir_name, lift_dir_name, sub_dir_name, batch_name, run_method):
-        # initialize the checks class/methods
-        output_checks = OutputChecks()
-
-        # remove the previous submission files
-        os.system (
-            f"rm -rf {output_dir}/{sub_dir_name}"
-        )
-        assert not os.path.exists(f"{output_dir}/{sub_dir_name}")
-
-        # call the submission entrypoint
-        os.system (
-            f"nextflow run main.nf -profile test,{run_method} -entry only_submission --submission_wait_time 2 --output_dir {output_dir} " + \
-            f"--submission_only_meta {self.root_dir}/{output_dir}/{meta_dir_name}/*/tsv_per_sample/ --submission_only_fasta {self.root_dir}/{output_dir}/{lift_dir_name}/*/fasta/ " + \
-            f"--submission_only_gff {self.root_dir}/{output_dir}/{lift_dir_name}/*/liftoff/ --submission_output_dir {sub_dir_name} --batch_name {batch_name} " + \
-            f"--submission_database all"
-        )
-        assert os.path.exists(f"{output_dir}/{sub_dir_name}")
-    
-    @staticmethod
-    def read_file_lines(path_to_file):
-        lines = open(glob.glob(path_to_file)[0], "r")
-        lines = [x.strip() for x in lines]
-        return lines
-    
-    @staticmethod
-    def remove_files(directory_to_remove):
-        os.system (
-            f"rm -rf {directory_to_remove}"
-        )
-        assert not os.path.exists(f"{directory_to_remove}")
-        
 
 
 if __name__ == "__main__":
